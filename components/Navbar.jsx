12ccabb82f18d8cc9d4c01c39e76b3cbf3b5dbc9
theMomo93 <mkaminska93@gmail.com>
2024-03-26 14:27:04 +0100

    about me

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,15 +6,18 @@ import portfolioImage from "../images/Aboutus.png";
 import { LiaLinkedin } from "react-icons/lia";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
-import {Ubuntu} from "next/font/google"
+import { Ubuntu } from "next/font/google";
 
 // Import the specific Google Font you want to use
-const ubuntu = Ubuntu({subsets:["latin"],
-weight:["400"]})
+const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["400"] });
 
 export default function Navbar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const sidebarRef = useRef(null);
+
+
+
+
 
   useEffect(() => {
     function handleClickOutside(event) {
@@ -34,57 +37,55 @@ export default function Navbar() {
   };
 
   return (
-    <div className={ubuntu.className}>
-      <button onClick={toggleSidebar} aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+    <div className={ubuntu.className} >
+      <button onClick={toggleSidebar} aria-controls="default-sidebar" type="button" className="fixed top-0 left-0 z-50 inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg bg-gray-200 ring-2  focus:outline-none focus:ring-2 focus:ring-gray-600 dark:text-teal-900 dark:hover:bg-gray-700 hover:text-white ring-black dark:focus:ring-gray-600">
         <span className="sr-only">Open sidebar</span>
         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
         </svg>
       </button>
 
-      <aside ref={sidebarRef} id="default-sidebar" className={`fixed top-0 left-0 z-40 w-96 h-screen transition-transform ${isSidebarOpen ? '' : '-translate-x-full sm:translate-x-0'}`} aria-label="Sidebar">
-        <div className="h-full px-4 py-4 overflow-y-auto bg-gray-50 dark:bg-gradient-to-b from-yellow-100 to-teal-900 flex flex-col ">
+      <aside ref={sidebarRef} id="default-sidebar" className={`fixed top-0 left-0 z-40 w-96 h-screen transition-transform ${isSidebarOpen ? '' : '-translate-x-full'}`} aria-label="Sidebar ">
+        <div className="h-full px-4 py-4 overflow-y-auto bg-gray-50 dark:bg-gradient-to-b from-gray-600 to-teal-600 flex flex-col ">
           <Image
-            className='my-4 mx-auto rounded-full sm:w-40 sm:h-40 md:w-48 md:h-48 border-solid border-orange-200 border-2 flex justify-center'
+            className='my-4 mx-auto rounded-full sm:w-40 sm:h-40 md:w-48 md:h-48 border-solid border-gray-200 border-2 flex justify-center'
             src={portfolioImage}
             alt="applicant's photo"
             width={220} height={220}
           />
           <h1 className='text-gray-100 text-5xl font-semibold  '>Monika Genedl</h1>
           
-          <p className='text-gray-100 text-xl flex justify-center mb-10 text-orange-200'>Junior Fullstack Developer</p>
+          <p className='text-gray-100 text-xl flex justify-center mb-10 text-gray-900'>Junior Fullstack Developer</p>
 
-          <h2 className='font-semibold text-2xl text-gray-200 flex align-center justify-center'>I'm looking for... </h2>
+          <h2 className='font-semibold text-2xl text-gray-900 flex align-center justify-center'>I'm looking for... </h2>
           <hr/>
           <ul className='text-gray-100 text-xl flex flex-col  justify-center align-center my-6'>
-            <li className='cursor-pointer flex m-1 justify-center bg-gray-100 text-black font-semibold rounded-full hover:bg-orange-200 p-2'><Link href="/about">About</Link></li>
-            <li className='cursor-pointer flex m-1 justify-center bg-gray-100 text-black font-semibold rounded-full hover:bg-orange-200 p-2'><Link href="/skills">Hard Skills</Link></li>
-            <li className='cursor-pointer flex m-1 justify-center bg-gray-100 text-black font-semibold rounded-full hover:bg-orange-200 p-2'><Link href="/skills">Soft Skills</Link></li>
+            <li ><a className='cursor-pointer flex m-1 justify-center bg-gray-100 text-black font-semibold rounded-full hover:bg-gray-700 hover:text-gray-100 p-2'  href="#about">About</a></li>
+            <li ><a className='cursor-pointer flex m-1 justify-center bg-gray-100 text-black font-semibold rounded-full hover:bg-gray-700 hover:text-gray-100 p-2' href="#techskills">Tech Skills</a></li>
+            <li ><a className='cursor-pointer flex m-1 justify-center bg-gray-100 text-black font-semibold rounded-full hover:bg-gray-700 hover:text-gray-100 p-2' href="#sofskills">Soft Skills</a></li>
 
-            <li className='cursor-pointer flex m-1 justify-center bg-gray-100 text-black font-semibold rounded-full hover:bg-orange-200 p-2'><Link href="/work">Portfolio</Link></li>
+            <li ><a className='cursor-pointer flex m-1 justify-center bg-gray-100 text-black font-semibold rounded-full hover:bg-gray-700 hover:text-gray-100 p-2' href="#work">Portfolio</a></li>
             
           </ul>
           <div className='text-gray-200'>
-  <h1 className='font-semibold text-2xl text-gray-200 flex justify-center'> Contact me!</h1>
-  <hr className='divide-dotted'/>
-  <div className='my-4'>
-    <p className='text-xl'><span className='font-semibold text-orange-200'>Telephone:</span> 01783090202 </p>
-    <p className='text-xl'><span className='font-semibold text-orange-200'>Email:</span> mkaminska93@gmail.com</p>
-  </div>
- 
-  <div className='flex'>
-  <a href="https://www.linkedin.com/in/monika-genedl-2934611b2/" target="_blank" rel="noopener noreferrer">
-    <LiaLinkedin className='text-7xl m-4 p-2 hover:bg-blue-700 rounded-full'/>
-  </a>
-  <a href="https://github.com/theMomo93" target="_blank" rel="noopener noreferrer">
-    <FaGithub className='text-7xl m-4 p-2 hover:bg-black rounded-full' />
-  </a>
-  <a href="https://www.instagram.com/mknika21/" target="_blank" rel="noopener noreferrer">
-    <FaInstagram className='text-7xl m-4 p-2 hover:bg-gradient-to-r from-yellow-500 via-red-500 to-blue-600 rounded-full' />
-  </a>
-</div>
-
-</div>
+            <h1 className='font-semibold text-2xl text-gray-900 flex justify-center'> Contact me!</h1>
+            <hr className='divide-dotted'/>
+            <div className='my-4'>
+              <p className='text-xl'><span className='font-semibold text-gray-900'>Telephone:</span> 01783090202 </p>
+              <p className='text-xl'><span className='font-semibold text-gray-900'>Email:</span> mkaminska93@gmail.com</p>
+            </div>
+            <div className='flex'>
+              <a href="https://www.linkedin.com/in/monika-genedl-2934611b2/" target="_blank" rel="noopener noreferrer">
+                <LiaLinkedin className='text-7xl m-4 p-2 hover:bg-blue-700 rounded-full'/>
+              </a>
+              <a href="https://github.com/theMomo93" target="_blank" rel="noopener noreferrer">
+                <FaGithub className='text-7xl m-4 p-2 hover:bg-black rounded-full' />
+              </a>
+              <a href="https://www.instagram.com/mknika21/" target="_blank" rel="noopener noreferrer">
+                <FaInstagram className='text-7xl m-4 p-2 hover:bg-gradient-to-r from-yellow-500 via-red-500 to-blue-600 rounded-full' />
+              </a>
+            </div>
+          </div>
         </div>
       </aside>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,103 @@
-import Navbar from '@/components/Navbar'
+import Navbar from "@/components/Navbar";
+import { Rubik } from "next/font/google";
+import { useRef } from "react";
+
+const rubik = Rubik({ subsets: ["latin"], weight: ["400"] });
 
 export default function Home() {
-  return (
-    <div className='ml-96'>
-      <Navbar />
-      <section className='px-24 py-8 p-4 bg-gray-100 h-screen'>
-        <h1 className='text-black '>Main Content</h1>
-        <div className='mt-8'>
-          <h2 className='text-xl font-semibold'>About Me</h2>
-          <p className='text-gray-700'>
-            Text
-          </p>
+  
+  const aboutRef = useRef(null);
+
+
+  const scrollToAbout = () => {
+    aboutRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+  
+
+
+  return(
+<div className="mx-auto sm:ml-0">
+      
+      <div className={rubik.className}>
+    
+      <section className=" sm:px-6 lg:px-24 bgImage h-fit w-screen "> 
+             
+            <Navbar />
+           
+            
+          
+          <div className="inline-flex items-center justify-center w-full mt-4 sm:mt-12">
+            <hr className="w-2/3 sm:w-3/4 h-1 my-4 sm:my-8 bg-orange-200 border-0 dark:bg-gray-300" />
+            <span 
+             id="about"
+             className="p-2 absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-gray-800 dark:bg-gray-300 text-black rounded text-3xl sm:text-5xl bg-opacity-60">
+              About me
+            </span>
+          </div>
+<span className="justify-center flex text-white font-semibold text-xl">The how and the why</span>
+          <div className="mt-4 sm:mt- backdrop-blur-xl p-6 sm:p-12 rounded-xl p-4 sm:p-8 shadow-2xl ring-1 ring-black/5 sm:m-2 h-auto">
+  <div
+    className="mx-36 text-gray-200 text-sm sm:text-lg lg:text-xl indent-4 sm:indent-8 font-semibold sm:font-light"
+    
+  >
+              <p>
+                I like challenge. I'm not promising I won't make mistakes, but I
+                will try my best to prevent them, learn from them, and apologize
+                for them. I am eager to learn more about coding. I know that
+                with a little time and a chance, I will be a valuable employee.
+              </p>
+              <hr className="bg-gray-900 border-none rounded h-1 w-64 mx-auto my-2" />
+              <p>
+                My career had ups and downs. I traveled most of my 20s around
+                Europe for World Rowing Competitions, meeting many different
+                people and experiencing foreign languages and countries. From a
+                world athlete, due to an injury, I had to switch my path. After
+                working hard and climbing the ladder from Housekeeping in a
+                foreign country through an associate in a logistics company to a
+                Team Leader at Amazon, I found out I had cancer. After fighting
+                it from 2021 to 2023 and trying to keep myself sane with online
+                Bootcamp, in March 2024, I finally managed to finish both of
+                them.
+              </p>
+              <hr className="bg-gray-900 border-none rounded h-1 w-64 mx-auto my-2" />
+              <p>
+                Free of cancer and with a new goal in mind, I am ready to climb
+                the ladder again, determined not to fall off it again. The
+                journey isn't over yet, but so far, I have learned so much, and
+                I want to learn even more in the future. If that doesn't prove
+                determination and stubbornness to achieve goals, then I don't
+                know what will. Right now, my eyes are set on Full-stack Web
+                Development.
+              </p>
+              <hr className="bg-gray-900 border-none rounded h-1 w-64 mx-auto my-2" />
+              <p>
+                I enjoy meeting new people from different backgrounds. Working
+                in development gives me the opportunity to create as well as
+                solve complex issues. The flexibility with a hybrid job is also
+                a big plus. I know I am going to be a great addition to your
+                company, and if you are still reading this, so do you.
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+      <section className="h-screen bg-gray-300">
+        <div
+        
+        id="techskills">
+        <h1>Tech Skills</h1>
+
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
         </div>
       </section>
     </div>
   );
-};
+}
