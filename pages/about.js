aa9d54ceb6eef1867ad86817467344630b8857a8
theMomo93 <mkaminska93@gmail.com>
2024-04-19 15:59:25 +0200

    about me

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div>
+        <div class="min-h-screen bg-gray-900 flex flex-col items-center justify-center relative px-4">
+    <div class="absolute top-0 left-0 w-full h-full bg-cover bg-center opacity-30"
+        style="background-image: url('https://images.unsplash.com/photo-1604093882750-3ed498f3178b');">
+    </div>
+    <h1 class="text-5xl md:text-7xl text-white font-bold mb-8 z-10">Coming Soon</h1>
+    <p class="text-white text-xl md:text-2xl">
+        We're working hard to bring you something amazing. Stay tuned!
+    </p>
+</div>
+    </div>
+  )
+}
