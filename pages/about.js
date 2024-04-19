9ca509c00f507bc18be0b54fce6c31d6340adbb5
theMomo93 <mkaminska93@gmail.com>
2024-04-19 16:01:17 +0200

    coming soon

--- pages/about.js
+++ pages/about.js
@@ -4,8 +4,7 @@ export default function about() {
   return (
     <div>
         <div class="min-h-screen bg-gray-900 flex flex-col items-center justify-center relative px-4">
-    <div class="absolute top-0 left-0 w-full h-full bg-cover bg-center opacity-30"
-        style="background-image: url('https://images.unsplash.com/photo-1604093882750-3ed498f3178b');">
+    <div class="absolute top-0 left-0 w-full h-full bg-cover bg-center opacity-30">
     </div>
     <h1 class="text-5xl md:text-7xl text-white font-bold mb-8 z-10">Coming Soon</h1>
     <p class="text-white text-xl md:text-2xl">
